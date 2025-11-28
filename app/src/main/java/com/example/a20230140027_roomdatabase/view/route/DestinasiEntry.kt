34afae4953459dc0002968b02b9dab91eeb8f12a
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-28 18:57:00 +0700

    membuat object DestinasiEntry.kt Object DestinasiEntry berfungsi sebagai identitas terpusat untuk halaman input data siswa yang menyimpan alamat navigasi (route) dan judul halaman (titleRes) dalam satu tempat yang aman.

---
+++ app/src/main/java/com/example/a20230140027_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.a20230140027_roomdatabase.view.route
+
+import com.example.a20230140027_roomdatabase.R
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
