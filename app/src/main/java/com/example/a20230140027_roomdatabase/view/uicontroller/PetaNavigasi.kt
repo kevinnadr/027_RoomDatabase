f3d305879662757e6640ebfdc0e050013cb42761
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-28 19:02:39 +0700

    Fungsi HostNavigasi berperan sebagai peta jalan aplikasi yang mengatur alur perpindahan layar. Ia menetapkan halaman utama sebagai titik awal, mengarahkan pengguna ke halaman input data, dan menangani perintah kembali ke layar sebelumnya.

--- app/src/main/java/com/example/a20230140027_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a20230140027_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -17,3 +17,24 @@ fun SiswaApp(navController: NavHostController = rememberNavController(),
           modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route
+        , modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry
+                    .route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
+}
