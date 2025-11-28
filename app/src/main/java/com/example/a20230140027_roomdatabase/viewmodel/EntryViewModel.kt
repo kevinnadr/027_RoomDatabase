98723873d6b70676887fdd9efa1a23dd8bfc155f
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-28 19:04:25 +0700

    membuat class EntryViewModel dan isi menggunakan uistatesiswa untuk melihat status dan buat fungsi untuk memvalidasi input

---
+++ app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.a20230140027_roomdatabase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.a20230140027_roomdatabase.repositori.RepositoriSiswa
+import com.example.a20230140027_roomdatabase.room.Siswa
+
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
+{
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /** Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
