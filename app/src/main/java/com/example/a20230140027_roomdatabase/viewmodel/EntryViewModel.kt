6a63719b432ffa4281f18d285e8b6c6fbd20f6f1
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-28 19:06:10 +0700

    membuat lagi fungsi untuk menyimpan data yang di entry dan buat data class untuk status ui siswa dan detail siswa lalu buat Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya

--- app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/EntryViewModel.kt
@@ -23,3 +23,53 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid =
+                validasiInput(detailSiswa))
+    }
+
+    /** Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
+/**
+ * Mewakili Status UI untuk Siswa
+ */
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+/** Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
