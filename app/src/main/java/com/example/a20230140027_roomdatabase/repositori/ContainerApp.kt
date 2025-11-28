9c2bc29cbee22ccaaebe477a9ca5a44f8e548aab
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-28 18:46:59 +0700

    membuat val interface ContainerApp

---
+++ app/src/main/java/com/example/a20230140027_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,9 @@
+package com.example.a20230140027_roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.a20230140027_roomdatabase.room.DatabaseSiswa
+
+interface  ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
