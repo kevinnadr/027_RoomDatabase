5bdc7801b9e0267c395d7c1d013a732959c013b0
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-28 19:11:10 +0700

    PenyediaViewModel berfungsi sebagai pabrik (factory) penghubung yang bertugas membuat HomeViewModel dan EntryViewModel sekaligus menyuplai (menyuntikkan) akses database kepada mereka. Kode ini memastikan bahwa setiap kali ViewModel dibuat, mereka langsung memiliki sambungan ke repositori data aplikasi sehingga bisa membaca atau menyimpan data siswa.

---
+++ app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.a20230140027_roomdatabase.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a20230140027_roomdatabase.repositori.AplikasiSiswa
+import com.example.a20230140027_roomdatabase.viewmodel.EntryViewModel
+import com.example.a20230140027_roomdatabase.viewmodel.HomeViewModel
+
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+ * instance dari
+ * [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
