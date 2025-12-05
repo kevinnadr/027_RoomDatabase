be53c542415123badecdff17008d69f47f5bcc58
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-05 19:39:17 +0700

    membuat suspend fun untuk mengambil, hapus dan update data siswa

--- app/src/main/java/com/example/a20230140027_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a20230140027_roomdatabase/repositori/RepositoriSiswa.kt
@@ -7,21 +7,17 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
-
     fun getSiswaStream(id: Int): Flow<Siswa?>
-
     suspend fun deleteSiswa(siswa: Siswa)
-
     suspend fun updateSiswa(siswa: Siswa)
 }
 
-class OffLineRepositoriSiswa(
+class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-): RepositoriSiswa{
+): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa)  =siswaDao.insert(siswa)
-
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-    override suspend fun updateSiswa(siswa: Siswa) =siswaDao.update(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
