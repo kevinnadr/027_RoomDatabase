5bee425f1c266d17c390f33cddd4c75e6d879165
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-05 15:25:00 +0700

    membuat file DetailViewModel.kt dan membuat fungsi yang berisi 2 dependency yaitu SavedStateHandle yang berfungsi menyimpan data dan RepositoriSiswa adalah tempat ViewModel mengambil atau mengelola data siswa dari database atau sumber data lain.

---
+++ app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/DetailViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.a20230140027_roomdatabase.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a20230140027_roomdatabase.repositori.RepositoriSiswa
+import com.example.a20230140027_roomdatabase.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel(){}
