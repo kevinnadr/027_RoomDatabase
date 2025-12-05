5d76ab65010655e683e276e20236c73c6d7cb164
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-05 19:31:44 +0700

    Menambahkan tanda tanya

--- app/src/main/java/com/example/a20230140027_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a20230140027_roomdatabase/room/SiswaDao.kt
@@ -10,19 +10,19 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
-
-    @Query(value = "SELECT * from tblSiswa WHERE id = :id")
+    // Edit : tambah 3 fungsi berikut
+    @Query("SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
-
-    @Delete
-    suspend fun delete(siswa: Siswa)
 
     @Update
     suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
