90eed39f01a51ae30f67d501fa5ecc1975c38f18
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-05 15:29:59 +0700

    membuat kode yang berfungsi untuk menyediakan konstanta dan struktur data UI yang dipakai ViewModel.

--- app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/a20230140027_roomdatabase/viewmodel/DetailViewModel.kt
@@ -32,3 +32,15 @@ class DetailViewModel (
     suspend fun deleteSiswa(){
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class DetailSiswaUiState(
+    val detailSiswa: DetailSiswa = DetailSiswa()
+)
