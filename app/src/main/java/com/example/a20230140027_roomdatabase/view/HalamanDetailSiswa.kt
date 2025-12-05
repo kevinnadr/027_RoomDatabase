edbe9d62e6d9201b6e61d6062ecef5fb4618e93d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-12-05 15:38:54 +0700

    membuat Kode yang berfungsi untuk membangun struktur tampilan layar menggunakan Scaffold, lengkap dengan top bar dan tombol aksi mengambang (FloatingActionButton) yang terhubung dengan data dari DetailViewModel.

--- app/src/main/java/com/example/a20230140027_roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/a20230140027_roomdatabase/view/HalamanDetailSiswa.kt
@@ -51,4 +51,28 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetailSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.uiDetailState.collectAsState()
+            FloatingActionButton(
+                onClick = {
+                    //navigateToEditItem(uiState.value.detailSiswa.id)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        },
